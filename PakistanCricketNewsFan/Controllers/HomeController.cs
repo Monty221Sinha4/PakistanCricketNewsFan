fea19acdece9ddd47ef018dde39cb1f16e63c97a
Ritesh Sinha <Ritesh.Sinha@mLogica.com>
2022-01-14 20:43:03 +0000

    I have added the CSS and JS to the site

---
+++ PakistanCricketNewsFan/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PakistanCricketNewsFan.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult History()
+        {
+            return View();
+        }
+        public ActionResult CurrentTeam()
+        {
+            return View();
+        }
+    }
+}
